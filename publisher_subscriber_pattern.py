8da0c96670b48c5bc611751c5f41bcb63f78730e
BXM22 <brennen@csuf.fullerton.edu>
2024-12-02 17:32:22 -0800

    implemented pub-sub strat
    just need to make it read the json data  and update it real time

--- publisher_subscriber_pattern.py
+++ publisher_subscriber_pattern.py
@@ -1,1 +1,32 @@
-print("hi")
+import queue
+ 
+class Publisher:
+    def __init__(self):
+        self.message_queue = queue.Queue()
+        self.subscribers = []
+ 
+    def subscribe(self, subscriber):
+        self.subscribers.append(subscriber)
+ 
+    def publish(self, message):
+        self.message_queue.put(message)
+        for subscriber in self.subscribers:
+            subscriber.receive(message)
+ 
+class Subscriber:
+    def __init__(self, name):
+        self.name = name
+ 
+    def receive(self, message):
+        print(f"{self.name}"+"received message:"
+              +f"{message}")
+ 
+publisher = Publisher()
+ 
+subscriber_1 = Subscriber("Subscriber 1")
+subscriber_2 = Subscriber("Subscriber 2")
+ 
+publisher.subscribe(subscriber_1)
+publisher.subscribe(subscriber_2)
+ 
+publisher.publish("Hello World")
